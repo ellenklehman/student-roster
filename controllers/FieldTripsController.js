90609c5a08b2e3c1bb8d9d8ddaac07ebb852defe
Ellen Krause Lehman <student@epicodus.com>
2015-02-16 16:36:03 -0800

    add permission list and non-permission list

--- controllers/FieldTripsController.js
+++ controllers/FieldTripsController.js
@@ -1,6 +1,9 @@
-studentRoster.controller('FieldTripsCtrl', function FieldTripsCtrl($scope) {
-  $scope.students=[];
-  $scope.permissionSlip= function() {
-    $scope.students.push({ name: $scope.studentName });
+studentRoster.controller('FieldTripsCtrl', function FieldTripsCtrl($scope, StudentsFactory) {
+  $scope.students = StudentsFactory.students;
+  $scope.studentsWithPermission=[];
+  $scope.permissionSlip = function(student) {
+    var index = $scope.students.indexOf(student);
+    $scope.studentsWithPermission.push(student);
+    $scope.students.splice(index, 1);
   };
 });
